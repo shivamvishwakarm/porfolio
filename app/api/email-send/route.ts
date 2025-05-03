94a5ad33a5cdba4d069ac954bf3bc0b855711de4
shivam vishwakarma <vishwakarmashivam2003@gmail.com>
2025-05-03 22:16:18 +0530

    commented email-send route

--- app/api/email-send/route.ts
+++ app/api/email-send/route.ts
@@ -1,23 +1,24 @@
-import { EmailTemplate } from '@/components/email-template';
-import { Resend } from 'resend';
+// import { EmailTemplate } from '@/components/email-template';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY) || "";
 
 export async function POST() {
-    try {
-        const { data, error } = await resend.emails.send({
-            from: 'test <vishwakarmashivam2003@gmail.com>',
-            to: ['vishwakarmashivam2003@gmail.com'],
-            subject: 'Hello world',
-            react: await EmailTemplate({ firstName: 'John' }),
-        });
+    return Response.json({ message: 'Hello World' });
+    // try {
+    //     const { data, error } = await resend.emails.send({
+    //         from: 'test <vishwakarmashivam2003@gmail.com>',
+    //         to: ['vishwakarmashivam2003@gmail.com'],
+    //         subject: 'Hello world',
+    //         react: await EmailTemplate({ firstName: 'John' }),
+    //     });
 
-        if (error) {
-            return Response.json({ error }, { status: 500 });
-        }
+    //     if (error) {
+    //         return Response.json({ error }, { status: 500 });
+    //     }
 
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error }, { status: 500 });
-    }
+    //     return Response.json(data);
+    // } catch (error) {
+    //     return Response.json({ error }, { status: 500 });
+    // }
 }
